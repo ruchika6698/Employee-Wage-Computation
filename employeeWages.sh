c3c02e269ccf35b8d7dd0ccd5099b3672f364e26
ruchika <ruchika6698@gmail.com>
2020-03-23 17:28:32 +0530

    Added:check Employee Attendance

--- employeeWages.sh
+++ employeeWages.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -x
 
-printf "-------Welcome to employee Wage problem-------\n"
+#constant
+PART_TIME_EMPLOYEE=4
+FULL_TIME_EMPLOYEE=8
+
+
+declare -A dailyWage
+echo "Welcome to Employee wage"
+
+function employeeAttendance() {
+	Number=$(( RANDOM%2 ))
+	if [ $Number -eq 1 ]
+	then
+		echo "Employee is present"
+	else
+		echo "Employee is absent"
+	fi
+}
+
+employeeAttendance
+
